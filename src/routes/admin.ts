805678332587ab117f0002823f98a286331612f1
Abhinav Shukla <149319019+StarLord824@users.noreply.github.com>
2025-01-17 09:50:20 +0000

    usermodel done

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,16 +1,18 @@
 import {Router} from "express"
 import {z} from "zod"
+import mongoose from "mongoose"
+import { UserModel } from "../model";
 
 const adminRouter: Router= Router();
 
-interface user{
-    name: z.ZodString
-}
+let user= z.object({
+    name: z.string().min(3).max(10),
+    password: z.string().min(8).regex(/[a-z]/).regex(/[A-Z]/).regex(/[0-9]/).regex(/[\W_]/).max(20)
+})
 
 adminRouter.post("/signup" , (req,res)=>{
-    const {
-        name:
-    }
+    let {name, password}=req.body;
+
 });
 
 adminRouter.post("/signin" , (req,res)=>{
